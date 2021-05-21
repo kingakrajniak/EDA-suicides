84df9a8b104ea02784d7a9bd7dc14f11f9c47308
kinga <krajniak.kinga@gmail.com>
2021-05-21 14:13:13 +0200

    Data fully loaded, the beginning

--- suicide_data_analysis.py
+++ suicide_data_analysis.py
@@ -3,5 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-suicide_dataset = pd.read_csv("master.csv")
-suicide_dataset.head(10)  # displaying top 10 rows
+df = pd.read_csv("master.csv")
+print(df.columns)
+
+pd.set_option('display.max_columns', 12)
+print(df.head(5))  # displaying top 10 rows
+print(df.dtypes)
+
+df = df.drop([' gdp_for_year ($) ', 'gdp_per_capita ($)'], axis=1)
+df = df.rename(columns={'country': 'Country',
+                        'year': 'Year',
+                        'sex': 'Sex',
+                        'age': 'Age',
+                        'suicides_no': 'Suicides no',
+                        'population': 'Population',
+                        'suicides/100k pop': 'Suicides per 100k pop',
+                        'country-year': 'Country and year',
+                        'generation': 'Generation'})
+print(df.columns)
+
+print(df.shape)
+df_duplicate_rows = df[df.duplicated()]
+print(df_duplicate_rows)  # no duplicate rows
+
+print(df.count())  # a lot of missing hdi data
+
+
--- suicide_dataset_info.sql
+++ suicide_dataset_info.sql
@@ -6,8 +6,8 @@ CREATE TABLE suicide_rates(country varchar(255),
                            population varchar(255),
                            suicides_per_100k_pop float(2),
                            country_year varchar(255),
-                           HDI_for_year float(3), 
-                           gdp_for_year_$ varchar(255),
+                           HDI_for_year varchar(255),  # dhi - human development index (long and healthy life, knowledge, decent standard of living)
+                           gdp_for_year_$ varchar(255),  # gdp - gross domestic product (monetary measure of the market value of all the final goods and services produced in a specific time period)
                            gdp_per_capita_$ int,
                            generation varchar(255));
 
@@ -17,9 +17,7 @@ LOAD DATA LOCAL INFILE 'C:\\Users\\Kinga\\Documents\\projects\\EDA-suicides\\mas
 INTO TABLE suicide_rates
 FIELDS TERMINATED BY ','
 	   OPTIONALLY ENCLOSED BY '"'
-      # ESCAPED BY '"'
-LINES STARTING BY '"'
-	 TERMINATED BY '\r'
+LINES TERMINATED BY '\r'
 IGNORE 1 ROWS;  # last three columns merging into gdp_for_year_$; how to fix it?
 # (country, year, sex, age, suicides_no, population, suicides_per_100k_pop, country_year, HDI_for_year, gdp_for_year_$, gdp_per_capita_$, generation);
 
@@ -28,3 +26,36 @@ SET GLOBAL local_infile=true;
 SHOW VARIABLES LIKE 'secure_file_priv';
 
 SELECT * FROM suicide_rates LIMIT 10;
+
+# suicides number in each country by year
+SELECT country, year, SUM(suicides_no) AS suicide_no_sum, HDI_for_year FROM suicide_rates GROUP BY country_year;
+# comparison
+SELECT country, year, SUM(suicides_no) AS suicide_no_sum FROM suicide_rates GROUP BY country, year;
+
+
+# checking hdi data
+SELECT country, 
+	   year, 
+       (SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country) AS no_hdi_data, 
+       COUNT(*) AS country_data,
+       ((SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country) / COUNT(*)) AS no_hdi_data_ratio
+FROM suicide_rates s2
+GROUP BY country;  # a looot of missing hdi data - should we use it?
+
+SELECT country, 
+	   year, 
+       (SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country AND year >= 2000) AS no_hdi_data, 
+       COUNT(*) AS country_data,
+       ((SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country AND year >= 2000) / COUNT(*)) AS no_hdi_data_ratio
+FROM suicide_rates s2
+WHERE year >= 2000
+GROUP BY country;
+
+SELECT country, 
+	   year, 
+       (SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country AND year >= 2010) AS no_hdi_data, 
+       COUNT(*) AS country_data,
+       ((SELECT COUNT(*) FROM suicide_rates s1 WHERE HDI_for_year LIKE '' AND s1.country = s2.country AND year >= 2010) / COUNT(*)) AS no_hdi_data_ratio
+FROM suicide_rates s2
+WHERE year >= 2010
+GROUP BY country;  # data improving with time
